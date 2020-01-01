67e5541e71a993c26daa18650d23b213ca437d62
MxxxL <49240218+MxxxL@users.noreply.github.com>
2020-01-01 22:26:28 +0800

    测试

--- service/src/main/java/com/ha/service/impl/UserServiceImpl.java
+++ service/src/main/java/com/ha/service/impl/UserServiceImpl.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     @Override
     public String testService() {
-        return null;
+        return "testService";
     }
 }
--- web/src/main/java/com/ha/web/controller/TestController.java
+++ web/src/main/java/com/ha/web/controller/TestController.java
@@ -1,5 +1,7 @@
-package ha.web.controller;
+package com.ha.web.controller;
 
+import com.ha.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class TestController {
+    @Autowired
+    private UserService userService;
+    @GetMapping(value = "/about")
+    public String about(){
+        return userService.testService();
+    }
     @GetMapping(value="/hello")
     public String hello(){
         return "hello";
